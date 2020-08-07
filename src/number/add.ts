505e442d2566737ff4be64cdc47ff0039aafead9
m2 <467057463@qq.com>
2020-08-07 17:18:54 +0800

    init sub 减法

--- src/number/add.ts
+++ src/number/add.ts
@@ -2,16 +2,20 @@ import decimalLength from '../internal/number/decimalLength';
 
 /**
  * 精确加法
- * @param value1
- * @param value2
- * @param othersValue
+ * @param num1 加数
+ * @param num2 加数
+ * @returns 返回相加的和
+ * @example
+ *  mmUtil.add(0.1, 0.2) //0.3  不是0.3000000001
+ * @example
+ *  mmUtil.add(1, 1, 2) //4  多个数相加
  */
-const add = (value1: number, value2: number, ...othersValue: number[]): number => {
-  if (othersValue.length) {
-    return add(add(value1, value2), othersValue[0], ...othersValue.slice(1));
+const add = (num1: number, num2: number, ...otherNums: number[]): number => {
+  if (otherNums.length) {
+    return add(add(num1, num2), otherNums[0], ...otherNums.slice(1));
   }
-  const baseNum = 10 ** Math.max(decimalLength(value1), decimalLength(value2));
-  return (value1 * baseNum + value2 * baseNum) / baseNum;
+  const baseNum = 10 ** Math.max(decimalLength(num1), decimalLength(num2));
+  return (num1 * baseNum + num2 * baseNum) / baseNum;
 };
 
 export default add;
--- src/number/sub.ts
+++ src/number/sub.ts
@@ -1,3 +1,19 @@
-export default (value: number, value1: number): number => {
-  return value - value1;
+import decimalLength from '../internal/number/decimalLength';
+
+/**
+ *
+ * @param num1 被减数
+ * @param num2 减数
+ * @returns 相减得到的差
+ * @example
+ *  1 - 0.9 = 0.1
+ *  5 - 1 - 1 = 3
+ */
+const sub = (num1: number, num2: number, ...otherNums: number[]): number => {
+  if (otherNums.length) {
+    return sub(sub(num1, num2), otherNums[0], ...otherNums.slice(1));
+  }
+  const baseNum = 10 ** Math.max(decimalLength(num1), decimalLength(num2));
+  return (num1 * baseNum - num2 * baseNum) / baseNum;
 };
+export default sub;
