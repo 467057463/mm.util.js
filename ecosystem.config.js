bbab2c53e62c74149ab93280fbf71f06ea61f467
m2 <467057463@qq.com>
2021-04-01 20:20:40 +0800

    init deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps : [
+    {
+      name: 'blog_front',
+      script: 'npm',
+      args: 'run start:prod',
+      env: {
+        "COMMON_VARIABLE": "true"
+      },
+      env_production: {
+        "NODE_ENV": "production" // 环境变量
+      }
+    }
+  ],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : '80.240.22.42',
+      ref  : 'origin/master',
+      repo : 'git@github.com:467057463/mm.util.git',
+      path : '/var/www/mm.util',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm --production=false install && npm run docs:build',
+      'pre-setup': '',
+      "env": {
+        "NODE_ENV": "production"
+      }
+    }
+  }
+};
