e98f5a0a1a8ad10ac575e68ed3991d9188a43bfc
m2 <467057463@qq.com>
2021-04-06 10:23:39 +0800

    添加 type() 类型判断方法

--- src/lang/type.ts
+++ src/lang/type.ts
@@ -1,8 +1,13 @@
 const reg = /\[object ([\w-]+)\]/;
 export default function type(obj: any):string {
   if(typeof obj === 'object' || typeof obj === 'function'){
-    const match = Object.prototype.toString.call(obj).match(reg)
-    return match ? match[1].toLowerCase() : 'object'
+    const match = Object.prototype.toString.call(obj).match(reg);
+    if(match){
+      const type = match[1]
+      return type[0].toLowerCase() + type.slice(1)
+    }else{
+      return 'object';
+    }
   }else{
     return typeof obj;
   }
